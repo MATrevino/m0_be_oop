a06f558024fcdf3bc66bd3eab2d7a1bdb8cf4cc8
Meredith Trevino <meredithtrevino@gmail.com>
2022-09-16 11:03:24 -0600

    Add oop work

--- oop.rb
+++ oop.rb
@@ -3,6 +3,18 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+  def initialize(name)
+    @name = name
+    @color = "silver"
+  end
+  def say(speech)
+     puts "*~* #{speech} *~*"
+  end
+end
+
+unicorn1 = Unicorn.new("Alan")
+unicorn1.say("Hello world")
 
 
 #  Write a class called Vampire
@@ -10,21 +22,20 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+  def initialize(name)
+    @name = name
+    @pet = "bat"
+    @thirsty = "true"
+  end
+  def drink
+    if @thirsty == true
+      @thirsty == false
 
+  end
+end
 
-#  Write a Dragon class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic rider attribute (string)
-#  it should have a dynamic color attribute (string)
-#  it should have a is_hungry attribute that is true by default
-#  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+vampire1= Vampire.new("Hank", "false")
+vampire1.new_drink
 
-
-#  Write a Hobbit class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic disposition attribute (string)
-#  it should have an age attribute that defaults to 0
-#  it should have a celebrate_birthday method. When called, the age increases by 1
-#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
-#  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
-#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+puts vampire1
